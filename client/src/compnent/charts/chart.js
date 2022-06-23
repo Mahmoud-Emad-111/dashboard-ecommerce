436f126b7abc6c75f11c102420e2006cc5d393c3
Mahmoud-Emad-111 <107431965+Mahmoud-Emad-111@users.noreply.github.com>
2022-06-23 03:43:33 +0200

    source code

--- client/src/compnent/charts/chart.js
+++ client/src/compnent/charts/chart.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./chart.css";
 import  { PureComponent } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import "import "~/node_modules/bootstrap/dist/css/bootstrap.min.css"";
+import  "~/node_modules/bootstrap/dist/css/bootstrap.min.css";
 const data = [
     {
       name: 'january',
